358a004438d85de24f109cee93f164d9f0050f19
LusterPearl <Salamioluwatosin90@gmail.com>
2024-06-15 20:58:00 +0100

    readj

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,17 +3,17 @@ const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
 
 class AppController {
-    static async getStatus(req, res) {
-        const dbAlive = dbClient.isAlive();
-        const redisAlive = redisClient.isAlive();
-        res.status(200).json({ redis: redisAlive, db: dbAlive });
-    }
+  static async getStatus(req, res) {
+    const dbAlive = dbClient.isAlive();
+    const redisAlive = redisClient.isAlive();
+    res.status(200).json({ redis: redisAlive, db: dbAlive });
+  }
 
-    static async getStats(req, res) {
-        const users = await dbClient.nbUsers();
-        const files = await dbClient.nbFiles();
-        res.status(200).json({ users, files });
-    }
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.status(200).json({ users, files });
+  }
 }
 
 module.exports = AppController;
--- utils/db.js
+++ utils/db.js
@@ -6,45 +6,45 @@ import { promisify } from 'util';
 const timeout = promisify(setTimeout);
 
 class DBClient {
-    constructor() {
-        // Get the connection parameters from environment variables or use defaults
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.DB_PORT || 27017;
-        const database = process.env.DB_DATABASE || 'files_manager';
+  constructor() {
+    // Get the connection parameters from environment variables or use defaults
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
 
-        // Create the MongoDB URI
-        const uri = `mongodb://${host}:${port}`;
+    // Create the MongoDB URI
+    const uri = `mongodb://${host}:${port}`;
 
-        // Create a MongoDB client
-        this.client = new MongoClient(uri, { useUnifiedTopology: true });
+    // Create a MongoDB client
+    this.client = new MongoClient(uri, { useUnifiedTopology: true });
 
-        // Connect to the MongoDB server
-        this.client.connect()
-            .then(() => {
-                this.db = this.client.db(database);
-                console.log('Connected to MongoDB');
-            })
-            .catch((err) => {
-                console.error('MongoDB connection error:', err);
-            });
-    }
+    // Connect to the MongoDB server
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(database);
+        console.log('Connected to MongoDB');
+      })
+      .catch((err) => {
+        console.error('MongoDB connection error:', err);
+      });
+  }
 
-    // Method to check if the connection is alive
-    isAlive() {
-        return this.client.isConnected();
-    }
+  // Method to check if the connection is alive
+  isAlive() {
+    return this.client.isConnected();
+  }
 
-    // Asynchronous method to get the number of users
-    async nbUsers() {
-        const collection = this.db.collection('users');
-        return collection.countDocuments();
-    }
+  // Asynchronous method to get the number of users
+  async nbUsers() {
+    const collection = this.db.collection('users');
+    return collection.countDocuments();
+  }
 
-    // Asynchronous method to get the number of files
-    async nbFiles() {
-        const collection = this.db.collection('files');
-        return collection.countDocuments();
-    }
+  // Asynchronous method to get the number of files
+  async nbFiles() {
+    const collection = this.db.collection('files');
+    return collection.countDocuments();
+  }
 }
 
 // Create and export an instance of DBClient
