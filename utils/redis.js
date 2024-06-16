b18b140cf3f825186656b3c13fca1b1d99fa37a6
LusterPearl <Salamioluwatosin90@gmail.com>
2024-06-16 18:49:54 +0100

    taskerr

--- utils/redis.js
+++ utils/redis.js
@@ -1,6 +1,5 @@
 // utils/redis.js
 const redis = require('redis');
-const { promisify } = require('util');
 
 class RedisClient {
   constructor() {
@@ -8,18 +7,10 @@ class RedisClient {
     this.client.on('error', (err) => {
       console.error('Redis client error:', err);
     });
-
-    this.pingAsync = promisify(this.client.ping).bind(this.client);
   }
 
-  async isAlive() {
-    try {
-      const response = await this.pingAsync();
-      return response === 'PONG';
-    } catch (err) {
-      console.error('Error checking Redis connection:', err);
-      return false;
-    }
+  isAlive() {
+    return this.client.connected;
   }
 
   async get(key) {
