b0934dd388839346a343aec5e94a128ee29aff87
LusterPearl <Salamioluwatosin90@gmail.com>
2024-06-16 22:46:46 +0100

    alm

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,21 +5,21 @@ class UsersController {
   static async postNew(req, res) {
     try {
       const { email, password } = req.body;
-
+  
       if (!email) {
         return res.status(400).json({ error: 'Missing email' });
       }
-
+  
       if (!password) {
         return res.status(400).json({ error: 'Missing password' });
       }
-
+  
       const newUser = await dbClient.createUser(email, password);
       return res.status(201).json(newUser);
     } catch (error) {
-      return res.status(500).json({ error: error.message });
+      return res.status(400).json({ error: error.message });
     }
   }
-}
+}  
 
 module.exports = UsersController;
