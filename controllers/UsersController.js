4d88a9ae6af250bedce73ed51a76a0e7301782fa
LusterPearl <Salamioluwatosin90@gmail.com>
2024-06-16 21:32:12 +0100

    200

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,21 +3,21 @@ const dbClient = require('../utils/db');
 
 class UsersController {
   static async postNew(req, res) {
-    const { email, password } = req.body;
-
-    if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
-    }
-
-    if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
-    }
-
     try {
+      const { email, password } = req.body;
+
+      if (!email) {
+        return res.status(400).json({ error: 'Missing email' });
+      }
+
+      if (!password) {
+        return res.status(400).json({ error: 'Missing password' });
+      }
+
       const newUser = await dbClient.createUser(email, password);
       return res.status(201).json(newUser);
-    } catch (err) {
-      return res.status(400).json({ error: err.message });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
     }
   }
 }
